73b03e5d808279b64c55421617a523e6e1417788
Csabesz75 <csabesz75@yahoo.com>
2021-08-02 15:24:12 +0300

    login first page

---
+++ web/themes/portal_theme/js/portal-theme.js
@@ -0,0 +1,21 @@
+/**
+ * @file
+ * Portal behaviors.
+ */
+
+(function ($, Drupal) {
+
+  'use strict';
+
+  /**
+   * Behavior description.
+   */
+  Drupal.behaviors.portalTheme = {
+    attach: function (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (jQuery, Drupal));
